512fda117a7200f212a11df8a32b9103f9381220
Janice Muia <janiceink001@gmail.com>
2020-06-09 23:27:35 +0300

    graphql config

--- hackernews/hackernews/settings.py
+++ hackernews/hackernews/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema'
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/

5bf22e20a5be5a4e22c256ca461e60f9e3e27ac9
Janice Muia <janiceink001@gmail.com>
2020-06-09 23:33:51 +0300

    links migzzy

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class link(models.Model):
+class Link(models.Model):
     url=models.URLField()
     description = models.TextField(blank=True)

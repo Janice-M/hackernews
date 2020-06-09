651a6f398356bae96d02fb39dcbf148ae1abd994
Janice Muia <janiceink001@gmail.com>
2020-06-10 00:12:37 +0300

    graphiql IDE in projects urls

--- hackernews/hackernews/urls.py
+++ hackernews/hackernews/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.crsf import csrf_exempt
-
+from  graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql',csrf_exempt(GraphQLView.as_view(graphiql=true))),
 ]

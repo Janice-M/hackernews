c45fd0d2ee3a165f7845b674313815a4d8d50a9b
Janice-M <janiceink001@gmail.com>
2020-06-25 23:51:24 +0300

    app

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Link(models.Model):
+class Link(models.del):
     url = models.URLField()
     description = models.TextField(blank=True)

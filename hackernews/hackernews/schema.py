f42bced6b7da1517d70c434080e67ec887bd9d94
Janice-M <janiceink001@gmail.com>
2020-06-29 23:58:21 +0300

    mutation in main changed

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -11,7 +11,7 @@ class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
 class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Fie
+    refresh_token = graphql_jwt.Refresh.Field
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)

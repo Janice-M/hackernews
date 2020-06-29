d2d13a82a7df182a9a90fb425776cabc8419e678
Janice-M <janiceink001@gmail.com>
2020-06-29 23:50:57 +0300

    link users to main schema

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -7,7 +7,9 @@ import users.schema
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(links.schema.Mutation, graphene.ObjectType)
-pass
 
-schema = graphene.Schema(query=Query, mutation= Mutation)
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType,):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)

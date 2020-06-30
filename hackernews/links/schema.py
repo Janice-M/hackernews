da36c527ead5a8f2a50843c2fed82fad256410b1
Janice-M <janiceink001@gmail.com>
2020-06-30 20:43:14 +0300

    adding author who created link to create link schema

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -24,6 +24,7 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field (UserType)
 
     #2
     class Arguments:

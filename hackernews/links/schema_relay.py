89262d45e157dad49e384ed6c7270261974155f7
Janice-M <janiceink001@gmail.com>
2020-07-03 21:27:16 +0300

    class link filter

--- hackernews/links/schema_relay.py
+++ hackernews/links/schema_relay.py
@@ -4,3 +4,9 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Link, Vote
+
+#1
+class LinkFilter(django_filters.FilterSet):
+    class Meta:
+        model = Link
+        fields = ['url', 'description']

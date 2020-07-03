7aaecbc328a111377a9d0276da66267840fc9e05
Janice-M <janiceink001@gmail.com>
2020-07-03 21:36:13 +0300

    mutation in main schema

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -14,7 +14,14 @@ class Query(
     pass
 
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+# ...code
+# schema mutation final code
+class Mutation(
+    users.schema.Mutation,
+    links.schema.Mutation,
+    links.schema_relay.RelayMutation,
+    graphene.ObjectType,
+):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
